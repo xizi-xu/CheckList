dfcdfcbb9fb6f7869b266cf285d650f24eb385ab
Liuxizi Xu <xlxzbisulyn@gmail.com>
2015-08-11 16:43:20 -0500

    model and api

--- server.js
+++ server.js
@@ -18,6 +18,48 @@ app.use(bodyParser.json())
 app.use(bodyParser.json({type: 'application/vnd.api+json'}))
 app.use(methodOverride())
 
+// define model
+var Todo = mongoose.model('Todo', {
+  text: String;
+})
+
+//routes - api
+// get all todos
+app.get('/api/todos', function(req, res) {
+  Todo.find(function(err, todos){
+    if (err) res.send(err)
+    //return all todos in JSON
+    res.json(todos)
+  })
+})
+// create todo and send back all todos after creation
+app.post('/api/todos', function(req, res) {
+  Todo.create({
+    text.req.body.text,
+    done: false
+  }, function(err, todo) {
+    if (err) res.send(err)
+    // get and return all the todos after creating
+    Todo.find(function(err, todos){
+      if (err) res.send(err)
+      res.json(todos)
+    })
+  })
+})
+// delete a todo
+app.delete('/api/todos/:todo_id', function(req, res){
+  Todo.remove({
+    _id: req.params.todo_id
+  }, function(err, todo) {
+    if (err) return res.send(err)
+    // get and return all the todos after deleting
+    Todo.find(function(err, todos) {
+      if (err) return res.send(err)
+      res.json(todo);
+    })
+  })
+})
+
 // listen
 app.listen(8080)
 console.log("App listening on port 8080. . .");
